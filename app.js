4c4070f4a29e502834dda7ba937207785cf0fd5c
xavijam <xavijam@gmail.com>
2013-04-27 04:06:26 +0200

    first steps done

--- app.js
+++ app.js
@@ -28,32 +28,32 @@ var message = {
   }]
 };
 
+// CartoDB client
+var client = new CartoDB({
+  user:     CONFIG.cartodb.user,
+  api_key:  CONFIG.cartodb.api_key
+});
 
-// Runs every Monday at 09:30:00 AM.
+client.connect();
+
+
+// Runs every Monday at 09:30:00 AM in production.
 // send the message and get a callback with an
 // error or details of the message that was sent
 var job = new cronJob({
   cronTime: CONFIG.cron.when,
   onTick: function() {
-    // CartoDB client
-    var client = new CartoDB({
-      user:     CONFIG.cartodb.user,
-      api_key:  CONFIG.cartodb.api_key
-    });
-
     // Get turn
     var actual_week = new Date().getWeekFrom(start_date);
     var turn = actual_week % pairs;
 
-    client.on('connect', function() {
-      client.query("SELECT * FROM cleaning_pairs WHERE turns=" + turn, {}, function(err, data){
-        // Send email
-        if (!err && data.rows && data.rows[0])
-          server.send(parseMessage(message, data.rows[0]), function(err, message) { console.log(err || message); });
-      });
+    client.query("SELECT * FROM cleaning_pairs WHERE turns=" + turn, {}, function(err, data){
+      // Send email
+      if (!err && data.rows && data.rows[0])
+        server.send(parseMessage(message, data.rows[0]), function(err, message) {
+          if (err) console.log(err);
+        });
     });
-
-    client.connect();
   },
   start: false,
   timeZone: "Europe/Madrid"
@@ -66,13 +66,35 @@ job.start();
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade'); 
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.compress());
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(app.router);
+  app.use(express.static('./public'));
 });
 
-app.listen(3000);
+app.get('/', function(req, res){
+  // Get turn
+  var actual_week = new Date().getWeekFrom(start_date);
+  var turn = actual_week % pairs;
+
+  client.query("SELECT * FROM cleaning_pairs WHERE turns=" + turn, {}, function(err, data){
+    if (err) data = { rows: [{}]};
+    res.render('home', data.rows[0]);
+  });
+});
+
+if (!module.parent) {
+  var port = process.env.PORT || 3000;
+  app.listen(port);
+  console.log('Vizziotica app started on port ' + port);
+}
 
 // Parsing message
 function parseMessage(msg, data) {
@@ -90,5 +112,5 @@ function parseMessage(msg, data) {
 // Date helper
 Date.prototype.getWeekFrom = function(date) {
   var onejan = new Date(date);
-  return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+  return Math.ceil((((this - onejan) / 86400000) + onejan.getDay())/7);
 }
