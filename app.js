a29cbb0a770388e019f21ba9a61bb1f809dd5628
xavijam <xavijam@gmail.com>
2013-06-12 14:02:08 +0200

    test for heroku

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var server  = email.server.connect({
   ssl:       true
 });
 
+console.log(new Date.getDate(), new Date.getMonth());
+
 // App vars
 var pairs       = 6;
 var start_date  = new Date('2013-04-22');
@@ -45,6 +47,7 @@ var job = new cronJob({
       for (var i = 0; i < guys.rows.length; i++) {
         var birth = new Date(guys.rows[i].birthday);
 
+        console.log(birth.getMonth() + "/" + birth.getDate(), today.getMonth() + "/" + today.getDate());
         if (birth.getMonth() == today.getMonth() && birth.getDate() == today.getDate()) {
           // Compose the data
           var d_ = guys.rows[i];
@@ -64,9 +67,11 @@ var job = new cronJob({
               alternative:  true
             }]
           };
-          server.send(message, function(err, msg) {
-            if (err) console.log(err);
-          });
+
+          return false;
+          // server.send(message, function(err, msg) {
+          //   if (err) console.log(err);
+          // });
         }
       }
     });
