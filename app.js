9e21412895c7b50fc85160b2b8618e1ef5f43763
xavijam <xavijam@gmail.com>
2013-12-02 11:11:04 +0100

    changed start date

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var server  = email.server.connect({
 
 // App vars
 var pairs       = 8;
-var start_date  = new Date('2013-04-15');
+var start_date  = new Date('2013-10-07');
 
 // Underscore 
 _u.templateSettings = {
